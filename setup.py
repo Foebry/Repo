98724bbd6e9ff902730ef527224fe27df4227b16
Foebry <rain_fabry@hotmail.Com>
2021-03-07 17:23:19 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'repo',
+    version = 1.0,
+    packages = [requests, argparse],
+    url = "https://github.com/Foebry/Repo",
+    licence = '',
+    author = "Foebry",
+    author_email = "rain_fabry@hotmail.com",
+    description = "Automation of repository creation."
+)
