d61001b656f293512cfae7e3364e54a05c17889f
Foebry <rain_fabry@hotmail.com>
2021-04-04 18:52:25 +0200

    removed TODO.txt from creation, auto-upgrade pip and variable env name

--- repo.py
+++ repo.py
@@ -17,6 +17,7 @@ url = "https://api.github.com/user/repos"
 if is_private: payload = '{"name": "' + repo_name + '", "private": true}'
 else: payload = '{"name": "' + repo_name + '", "private": false}'
 
+
 headers = {
     "Authorization": 'token '+GITHUB_TOKEN,
     "Accept": "application/vnd.github.v3+json"
@@ -27,14 +28,35 @@ response = requests.post(url, data=payload, headers=headers)
 
 try:
     REPO_PATH = os.getcwd()
-    print(REPO_PATH)
     os.chdir(REPO_PATH)
     os.system("mkdir " + repo_name)
-    os.chdir(f'{REPO_PATH}/{repo_name}')
+    parent_path = f'{REPO_PATH}/{repo_name}'
+    os.chdir(f'{parent_path}')
     os.system("git init")
     os.system("git remote add origin https://github.com/Foebry/" + repo_name)
-    os.system("echo '# " + repo_name + "' >> README.md")
-    os.system("echo '# " + repo_name + "' >> .gitignore")
-    os.system('python -m venv env')
+    os.system("echo. > README.md")
+    os.system(f'python -m venv {repo_name}')
+    # create .gitignore
+    os.system("echo __pycache__ >> .gitignore")
+    os.system("echo /Logs/* >> .gitignore")
+    os.system("echo *.log >> .gitignore")
+    # create setup.py
+    os.system("echo from setuptools import setup >> setup.py")
+    os.system("echo setup( >> setup.py")
+    os.system(f"echo     name = '{repo_name}', >> setup.py")
+    os.system("echo     version = 0.1, >> setup.py")
+    os.system("echo     packages = [], >> setup.py")
+    os.system(f"echo     url = 'https://github.com/Foebry/{repo_name}', >> setup.py")
+    os.system("echo     author = 'Foebry', >> setup.py")
+    os.system("echo     author_email = 'rain_fabry@hotmail.com', >> setup.py")
+    os.system("echo     description = '' >> setup.py")
+    os.system("echo ) >> setup.py")
+    # setting git settings
+    os.system('git config --global user.email "rain_fabry@hotmail.com"')
+    os.system('git config --global user.name "Foebry"')
     os.system('git add . && git commit -m "Initial commit" && git push origin master')
-except FileExistsError as error: pass
+    # adding parent_path to pythonpath environment variable
+    os.system(f"setx pythonpath='%pythonpath%;{parent_path}'")
+    os.system("python -m pip install --upgrade pip")
+except FileExistsError as error: print(f"A folder with name {repo_name} already exists")
+except Exception as error: print(type(error), error)
